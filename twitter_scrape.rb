1e9f0ae822734a966872701a105393a5b6674afd
Rosanne Hoyem <rosiehoyem@gmail.com>
2013-10-20 20:25:58 -0400

    built scraper

---
+++ twitter_scrape.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'nokogiri'
+
+domain_url = "https://twitter.com/ZenQuotes3"
+
+
+scrape_page = Nokogiri::HTML(open(domain_url))
+
+def get_data(scrape_page)
+    @data = scrape_page.css('p.tweet-text').text
+end
+
+def split_data
+  @zen_tweets = @data.split("#quotes")
+end
+
+def call_tweet
+  @zen_tweets[0]
+end
+
+get_data(scrape_page)
+split_data
+call_tweet
